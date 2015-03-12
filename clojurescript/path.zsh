a13c4a7647ac55e1dc41cc3952b0660f39b842c4
Justin Woo <moomoowoo@gmail.com>
2015-03-12 14:52:42 -0400

    get rid of complicated cljs install

--- clojurescript/path.zsh
+++
@@ -1,7 +0,0 @@
-# clojurescript
-export CLOJURESCRIPT_HOME="$HOME/Install/clojurescript"
-export PATH="$CLOJURESCRIPT_HOME/script:$CLOJURESCRIPT_HOME/bin:$PATH"
-# lein trampolines
-LEIN_FAST_TRAMPOLINE=y
-export LEIN_FAST_TRAMPOLINE
-alias cljsbuild="lein trampoline cljsbuild $@"
