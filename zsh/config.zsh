e004ecb34587b78dacf8817906c293b3108e0eac
Justin Woo <moomoowoo@gmail.com>
2015-03-25 20:26:44 -0400

    make this shit dumber and more like bash so that it's easier to use

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -21,15 +21,14 @@ setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
-setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
+setopt BASH_AUTO_LIST # bash style auto list
+
 setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
