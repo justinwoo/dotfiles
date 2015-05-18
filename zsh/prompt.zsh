8e44296c4f63e84ceeff2fd269adb7225923d121
Justin Woo <moomoowoo@gmail.com>
2015-05-18 00:11:09 -0500

    add a burger to my prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -50,7 +50,7 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'$(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'$(directory_name) $(git_dirty)$(need_push)🍔  '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
