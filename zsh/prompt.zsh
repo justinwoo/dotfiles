a0b2c78124c79121aff8c1ebd6f394f0a005f54f
Justin Woo <moomoowoo@gmail.com>
2015-08-09 20:27:12 +0900

    no more burger

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -40,9 +40,9 @@ unpushed () {
 need_push () {
   if [[ $(unpushed) == "" ]]
   then
-    echo " "
+    echo ""
   else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%}"
   fi
 }
 
@@ -50,7 +50,7 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'$(directory_name) $(git_dirty)$(need_push)🍔  '
+export PROMPT=$'$(directory_name) $(git_dirty)$(need_push) > '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
