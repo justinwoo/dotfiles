8f8c19c413c836f2e0a3d99bce4a625d676d161c
Justin Woo <moomoowoo@gmail.com>
2015-08-28 08:57:25 +0800

    reorder PATH order

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$PATH"
+export PATH="$PATH:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
