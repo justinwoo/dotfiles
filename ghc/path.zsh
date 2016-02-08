7fa43197bdc21125869a76f299b8b0b1b59c474c
Justin Woo <moomoowoo@gmail.com>
2016-02-08 15:41:14 +0900

    update some purescript and haskell crap

--- ghc/path.zsh
+++ ghc/path.zsh
@@ -1,5 +1,1 @@
 # Add GHC 7.10.2 to the PATH, via https://ghcformacosx.github.io/
-export GHC_DOT_APP="/Applications/ghc-7.10.2.app"
-if [ -d "$GHC_DOT_APP" ]; then
-  export PATH="${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${PATH}"
-fi
