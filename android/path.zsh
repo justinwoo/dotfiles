a29952c477b7f25e70da917a170fc0ddf27d722a
Justin Woo <moomoowoo@gmail.com>
2016-06-28 13:12:30 +0300

    fix android path issues

--- android/path.zsh
+++ android/path.zsh
@@ -1,2 +1,4 @@
 # If you installed the SDK via Homebrew, otherwise ~/Library/Android/sdk
-export ANDROID_HOME=/usr/local/opt/android-sdk
+export ANDROID_HOME=$HOME/Library/Android/sdk/
+export PATH=$PATH:$ANDROID_HOME:$ANDROID_HOME/platform-tools/
+
