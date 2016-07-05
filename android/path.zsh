6a05418e1b71e94a7f6d4e433fcb60241301a6b5
Justin Woo <moomoowoo@gmail.com>
2016-07-06 00:37:09 +0300

    setup computer for nuking

--- android/path.zsh
+++ android/path.zsh
@@ -1,4 +1,4 @@
 # If you installed the SDK via Homebrew, otherwise ~/Library/Android/sdk
-export ANDROID_HOME=$HOME/Library/Android/sdk/
-export PATH=$PATH:$ANDROID_HOME:$ANDROID_HOME/platform-tools/
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME:$ANDROID_HOME/platform-tools
 
--- postgres/aliases.zsh
+++ postgres/aliases.zsh
@@ -4,4 +4,7 @@ if test -f $POSTGRES_PLIST
 then
   alias pg.start="launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
   alias pg.stop="launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+else
+  alias pg.start="brew services start postgresql"
+  alias pg.stop="brew services stop postgresql"
 fi
