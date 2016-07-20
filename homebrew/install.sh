31555a65aa5272c029ccafe3e1a06ff2dfb9afc3
Justin Woo <moomoowoo@gmail.com>
2016-07-20 13:44:46 +0300

    crap

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -25,6 +25,7 @@ echo 'time to brew install'
 # Install homebrew packages - what the hell, default is so stupid
 # brew install grc coreutils spark
 brew install $(tr '\n' ' ' < $ZSH/homebrew/packages)
+brew install macvim --with-cscope --with-lua
 brew tap d12frosted/emacs-plus
 brew install emacs-plus --with-cocoa --with-gnutls --with-librsvg --with-imagemagick --with-spacemacs-icon
 brew linkapps
