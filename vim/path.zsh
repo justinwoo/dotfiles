0b703865971e5fad5095721ff0400cdd2246df95
Justin Woo <moomoowoo@gmail.com>
2016-09-15 11:35:53 +0200

    mayb ei need to set this

--- vim/path.zsh
+++ vim/path.zsh
@@ -1,2 +1,3 @@
 # because syntax doesn't know how to look for the correct path
-# export VIMRUNTIME=/usr/local/share/vim/vim74/
+export VIMRUNTIME=/usr/local/Cellar/neovim/0.1.5/share/nvim/runtime
+export VIM=$(which nvim)
