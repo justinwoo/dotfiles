ea01e2bf5736df6ac133744df2d36d069317de97
justinwoo <moomoowoo@gmail.com>
2016-10-26 18:50:17 +0200

    add yarn's global installs to path

--- node/path.zsh
+++ node/path.zsh
@@ -1,3 +1,5 @@
 export NODE_ENV='development'
 export PATH="$HOME/.npm/bin:$PATH"
 export PATH="./node_modules/.bin:./bin:$PATH"
+# use yarn's installed globals over npm's
+export PATH="/Users/jwoo/.yarn-cache/.global/node_modules/.bin:$PATH"
