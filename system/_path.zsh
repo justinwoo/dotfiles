5c30f8ae4da4184475f4baf1dd8202ac5c277a2f
justinwoo <moomoowoo@gmail.com>
2017-03-30 12:45:13 +0300

    why not just use lcaol/bin first

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="$HOME/.local/bin:$PATH:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin"
+export PATH="$HOME/.local/bin:./bin:/usr/local/bin:$PATH:/usr/local/sbin:$ZSH/bin:$HOME/.bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
