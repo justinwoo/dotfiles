2b76fe54250076e1426ec907b144280b66631ab5
justinwoo <moomoowoo@gmail.com>
2017-04-09 00:27:02 +0300

    update my aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,9 @@
 alias reload!='. ~/.zshrc'
 alias vi='vim'
 alias nv="mvim"
-alias gemacs='open -a Emacs'
-alias em='emacs'
-alias ge='gemacs $1'
+alias emd='emacs --daemon'
+alias em='emacsclient -t'
+alias ge='emacsclient -c'
 alias pb='pbcopy'
 alias chrome-remote-debug='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222'
 alias recent='ls -lht | head -10'
