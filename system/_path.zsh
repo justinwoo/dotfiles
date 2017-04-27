09fd70121342adcae179315403ca887f8a669ae1
justinwoo <moomoowoo@gmail.com>
2017-04-27 20:31:59 +0300

    yup

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="$HOME/.local/bin:./bin:/usr/local/bin:$PATH:/usr/local/sbin:$ZSH/bin:$HOME/.bin"
+export PATH="$HOME/.local/bin:./bin:$PATH:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
