e998801fce1e8f0b7e66af98e4859e89fd0ecb93
Justin Woo <moomoowoo@gmail.com>
2017-05-18 21:52:14 +0000

    yup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,13 +2,19 @@ alias reload!='. ~/.zshrc'
 alias vi='vim'
 alias nv="gvim"
 alias emd='emacs --daemon'
-alias emk='killall QUIT Emacs'
 alias em='emacsclient -t'
 alias ge='emacsclient -nc'
 alias pb='pbcopy'
 alias chrome-remote-debug='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222'
 alias recent='ls -lht --color | head -10'
 alias ll='lstoless'
+
+if test "$(uname)" = "Darwin"
+then
+  alias emk='killall QUIT Emacs'
+else
+  alias emk='killall emacs'
+fi
 
 if [[ -a $(which gxargs) ]]
 then
