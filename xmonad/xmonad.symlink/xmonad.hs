d589f726569036b17410be6020f6e42c0a933631
justinwoo <moomoowoo@gmail.com>
2017-10-18 15:48:14 +0300

    asdfasdlf

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,13 +1,30 @@
+import System.IO
 import XMonad
+import XMonad.Actions.CycleWS
+import XMonad.Actions.SpawnOn
+import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Accordion
+import XMonad.Layout.Named
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
+import XMonad.Prompt
+import XMonad.Prompt.Shell
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
+import XMonad.Util.SpawnOnce
+import XMonad.StackSet(greedyView)
+
+myStartupHook = do
+  spawnOnce "xmodmap ~/.Xmodmap"
+  spawnOnce "gnome-settings-daemon"
+
+myConfig = gnomeConfig
+  { startupHook = myStartupHook
+  , terminal = "gnome-terminal"
+  }
 
 main = do
-    mproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/.xmobarrc"
-    xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , startupHook = setWMName "Porkkanakone"
-        }
+  mproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/.xmobarrc"
+  xmonad myConfig
