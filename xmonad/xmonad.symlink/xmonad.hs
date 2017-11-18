1c94c23ace06f3c552c21ae618d757af1c4b82eb
justinwoo <moomoowoo@gmail.com>
2017-11-18 12:33:51 +0200

    bastardized setup

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,3 +1,4 @@
+import qualified Data.Map as M
 import System.IO
 import XMonad
 import XMonad.Actions.CycleWS
@@ -16,18 +17,72 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.SpawnOnce
 import XMonad.StackSet(greedyView)
 
+  
+myKeys x = [
+           -- , ((modMask x,               xK_Left),  prevWS)
+           -- , ((modMask x .|. shiftMask, xK_Right), shiftToNext)
+           -- , ((modMask x .|. shiftMask, xK_Left),  shiftToPrev)
+           -- , ((modMask x,               xK_t),     toggleWS)
+           -- , ((modMask x,               xK_s),     spawn "dmenu_run -b -fn 'Droid Sans Mono-14'")
+           -- , ((mod1Mask .|. controlMask, xK_l),    spawn "xtrlock -b")
+           -- , ((0                , 0x1008ff11),     spawn "amixer -c 1 sset Master 4-")
+           -- , ((0                , 0x1008ff13),     spawn "amixer -c 1 sset Master 4+")
+           -- , ((0                , 0x1008ff03),     spawn "xbacklight -inc -10%")
+           -- , ((0                , 0x1008ff02),     spawn "xbacklight -inc +10%")
+
+           -- , ((0                , 0x1008ff12),     spawn "amixer sset Master toggle")
+           ]
+
+newKeys x  = M.union (keys defaultConfig x) (M.fromList (myKeys x))
+
+myManageHook = composeAll (
+    [ manageHook gnomeConfig
+    , resource  =? "stalonetray" --> doIgnore
+    -- , className =? "Unity-2d-panel" --> doIgnore
+    -- , className =? "Unity-2d-launcher" --> doIgnore
+    -- , className =? "dota2" --> doIgnore
+    -- , className =? "DyingLightGame" --> doFloat doesn't work either way.
+    -- , className =? "Verdun.x86_64" --> doIgnore
+    -- , className =? "PillarsOfEternity" --> doIgnore
+    -- , className =? "hl2_linux" --> doIgnore
+    -- , className =? "eu4" --> doIgnore
+    -- , className =? "csgo_linux" --> doIgnore
+    -- , className =? "RocketLeague" --> doIgnore
+    -- , className =? "Gimp"      --> doFloat
+    -- , className =? "Vncviewer" --> doFloat
+    -- , className =? "Steam" --> doFloat
+    -- , className =? "steam" --> doIgnore -- bigpicture-mode
+    -- , className =? "MainThrd" --> doFloat
+    , manageDocks
+    ])
+
 myStartupHook = do
   spawnOnce "xmodmap ~/.Xmodmap"
-  spawnOnce "gnome-settings-daemon"
   spawnOnce "stalonetray --dockapp-mode simple"
+  spawnOnce "unity-settings-daemon"
+  spawnOnce "gnome-settings-daemon"
   spawnOnce "nm-applet"
+  spawnOnce "pasystray"
   spawnOnce "fdpowermon"
 
-myConfig = defaultConfig
-  { startupHook = myStartupHook
-  , terminal = "gnome-terminal"
-  }
+-- myLayoutHook = noBorders Full ||| noBorders (tabbed shrinkText defaultTheme) ||| Accordion
 
 main = do
-  mproc <- spawnPipe "/usr/bin/xmobar"
-  xmonad myConfig
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/callen/.xmobarrc"
+    xmonad $ gnomeConfig {
+         manageHook = myManageHook
+       , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
+       , logHook = dynamicLogWithPP xmobarPP
+                   { ppOutput = hPutStrLn xmproc
+                   , ppTitle = xmobarColor "green" "" . shorten 50
+                   }
+       , startupHook = myStartupHook
+       , modMask = mod4Mask
+       , keys = newKeys
+       , terminal = "gnome-terminal"
+       -- , terminal = "alacritty"
+       , handleEventHook =
+         mconcat [ docksEventHook
+                 , handleEventHook defaultConfig ]
+       -- , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
+       }
