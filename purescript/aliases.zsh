5529a5f51646e7d043f09deab22d7e92ffac95fe
justinwoo <moomoowoo@gmail.com>
2018-05-06 17:35:58 +0300

    osadifjoaweijf

---
+++ purescript/aliases.zsh
@@ -0,0 +1,2 @@
+alias use-purs-0.11.7='ln -sf $INSTALLED/purescript-0.11.7/purescript/purs ~/.bin/purs'
+alias use-purs-0.12.0-rc='ln -sf $INSTALLED/purescript-0.12.0-rc/purescript/purs ~/.bin/purs'
