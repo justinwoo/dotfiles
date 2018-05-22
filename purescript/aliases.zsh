0a96009755e785eded8ae613bf63e52e66bcf3ee
justinwoo <moomoowoo@gmail.com>
2018-05-23 01:13:51 +0300

    oijweafoijawef

--- purescript/aliases.zsh
+++ purescript/aliases.zsh
@@ -1,5 +1,4 @@
 alias use-purs-0.11.7='ln -sf $INSTALLED/purescript-0.11.7/purescript/purs ~/.bin/purs'
-alias use-purs-0.12.0-rc='ln -sf $INSTALLED/purescript-0.12.0-rc/purescript/purs ~/.bin/purs'
 alias use-purs-0.12='ln -sf $INSTALLED/purescript-0.12.0/purescript/purs ~/.bin/purs'
 
 alias bi='bower install'
