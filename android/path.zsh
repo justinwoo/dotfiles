23f339f769c41f9f8f149826c617811c57c035c4
justinwoo <moomoowoo@gmail.com>
2018-06-20 21:53:05 +0300

    shit

--- android/path.zsh
+++ android/path.zsh
@@ -1,4 +1,4 @@
 # If you installed the SDK via Homebrew, otherwise ~/Library/Android/sdk
-export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin
 
---
+++ flutter/path.zsh
@@ -0,0 +1,2 @@
+export FLUTTER_HOME="$INSTALLED/flutter"
+export PATH="$FLUTTER_HOME/bin:$PATH"
