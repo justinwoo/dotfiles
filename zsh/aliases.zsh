096367c5c666dd78c21910185015d8731ac76fc2
justinwoo <moomoowoo@gmail.com>
2018-07-25 09:51:10 +0300

    update enc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,6 @@ alias vi='nvim'
 alias nv="nvim"
 alias emd='emacs --daemon'
 alias em='emacsclient -t'
-alias enc='nix-shell -p --run "emacs . & disown" 2> /dev/null'
 alias ge='emacsclient -nc'
 alias pb='pbcopy'
 alias po='pbpaste'
