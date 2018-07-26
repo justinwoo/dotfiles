665d5b6b10cf1b6338e852b0a71eebc023f6029a
justinwoo <moomoowoo@gmail.com>
2018-07-26 11:54:53 +0300

    oewaijoiewajfewaf

--- liquidprompt/prompt.bash
+++ liquidprompt/prompt.bash
@@ -1,2 +1,12 @@
 # Only load Liquid Prompt in interactive shells, not from a script or from scp
-[ ! -z "$PS1" ] && source $INSTALLED/liquidprompt/liquidprompt;
+if [ ! -z "$PS1" ]
+then
+  source $INSTALLED/liquidprompt/liquidprompt;
+
+  if [ -z $IN_NIX_SHELL  ]
+  then
+    export LP_USER=bash
+  else
+    export LP_USER=nix
+  fi
+fi
