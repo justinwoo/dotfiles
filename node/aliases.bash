edec9703b1b57ce76e369035046eff011403874e
justinwoo <moomoowoo@gmail.com>
2018-08-09 16:04:16 +0300

    no more jest thank god

--- node/aliases.bash
+++ node/aliases.bash
@@ -1,7 +1,4 @@
 alias nr='npm run'
 alias nrb='npm run build'
 alias nrt='npm run test'
-alias jw='jest --watch'
-alias jwb='jest --watch --bail'
-alias jwbn='jest --watch --bail --no-coverage'
 alias fuckwebpack='pkill -f "webpack" -9'
