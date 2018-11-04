1c8b3336c4d68ed15e285b64c35e43a1b330546f
justinwoo <moomoowoo@gmail.com>
2018-11-04 16:27:24 +0200

    upgrade i3

--- nix/install.nix
+++ nix/install.nix
@@ -1,7 +1,18 @@
 let
   pkgs = import <nixpkgs> {};
   easy-ps = import ./easy-ps.nix;
+
 in {
+  i3 = pkgs.i3.overrideAttrs (old: rec {
+    name = "i3-${version}";
+    version = "4.16";
+
+    src = pkgs.fetchurl {
+      url = "https://i3wm.org/downloads/i3-4.16.tar.bz2";
+      sha256 = "1d2mnryn7m9c6d69awd7lwzadliapd0ahi5n8d0ppqy533ssaq6c";
+    };
+  });
+
   inherit (easy-ps.inputs)
     purs
     psc-package-simple
