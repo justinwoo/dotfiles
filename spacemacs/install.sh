3af9c018b42ce8b9ea9d8bf459ee23eeb1e8b550
justinwoo <moomoowoo@gmail.com>
2018-12-01 13:14:01 +0200

    oaijefoiwajf

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -3,4 +3,6 @@
 if [[ ! -a ~/.emacs.d ]]
 then
   git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d
+  cd ~/.emacs.d || echo "Didn't clone ~/.emacs.d correctly"
+  git co develop
 fi
