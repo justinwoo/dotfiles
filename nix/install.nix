1f3401df376a575f733b7ebe5638f613a39cf890
justinwoo <moomoowoo@gmail.com>
2018-12-09 14:40:39 +0200

    oiwjefoijewaf

--- nix/install.nix
+++ nix/install.nix
@@ -4,15 +4,17 @@ let
   easy-ps = import ./easy-ps.nix;
 
   i3-pkgs = {
-    i3 = pkgs.i3.overrideAttrs (old: rec {
-      name = "i3-${version}";
-      version = "4.16";
+    i3 = pkgs.i3;
+    # old overrides:
+    # i3 = pkgs.i3.overrideAttrs (old: rec {
+      # name = "i3-${version}";
+      # version = "4.16";
 
-      src = pkgs.fetchurl {
-        url = "https://i3wm.org/downloads/i3-4.16.tar.bz2";
-        sha256 = "1d2mnryn7m9c6d69awd7lwzadliapd0ahi5n8d0ppqy533ssaq6c";
-      };
-    });
+      # src = pkgs.fetchurl {
+      #   url = "https://i3wm.org/downloads/i3-4.16.tar.bz2";
+      #   sha256 = "1d2mnryn7m9c6d69awd7lwzadliapd0ahi5n8d0ppqy533ssaq6c";
+      # };
+    # });
 
     inherit (pkgs)
     scrot
