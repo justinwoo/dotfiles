00215ab32e200f3a8fc10452d5d8845bdcf44c19
justinwoo <moomoowoo@gmail.com>
2018-12-23 23:20:57 +0800

    oiwjefoiajef

--- purescript/aliases.bash
+++ purescript/aliases.bash
@@ -1,6 +1,3 @@
-alias use-purs-0.11.7='ln -sf $INSTALLED/purescript-0.11.7/purescript/purs ~/.bin/purs'
-alias use-purs-0.12='ln -sf $INSTALLED/purescript-0.12.0/purescript/purs ~/.bin/purs'
-
 alias bi='bower install'
 alias pp='psc-package'
 alias ppb='psc-package build'
