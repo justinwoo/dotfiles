a61c470fb836793327cebac8a54b5028379df469
justinwoo <moomoowoo@gmail.com>
2018-12-24 22:54:23 +0200

    somewhat fix spacemacs hell

--- nix/spacemacs.nix
+++ nix/spacemacs.nix
@@ -1,9 +1,19 @@
 #!/usr/bin/env nix-shell
-#!nix-shell ./spacemacs.nix --run "cp -r --no-preserve=mode,ownership,timestamp $SPACEMACS_SRC ~/.emacs.d"
+#!nix-shell ./spacemacs.nix --run exit
 
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.stdenv.mkDerivation {
+let
+  melpaPackages = import ./emacs-packages.nix { inherit pkgs; };
+  cpPackages = drv: ''
+    TARGET=~/.emacs.d/elpa/26.1/develop/
+    mkdir -p $TARGET
+    SITE_LOCATION=${drv.outPath}/share/emacs/site-lisp/elpa/*
+    cp -R --no-preserve=mode $SITE_LOCATION $TARGET
+  '';
+  copyPackages = builtins.map cpPackages (builtins.attrValues melpaPackages);
+
+in pkgs.stdenv.mkDerivation {
   name = "spacemacs-install";
   src = pkgs.fetchFromGitHub {
     owner = "syl20bnr";
@@ -11,7 +21,10 @@ pkgs.stdenv.mkDerivation {
     rev = "8b3a30f2409aa06053b32117504381c4cce1d8db";
     sha256 = "1z1gxm4vghm8h45drdb6s8lg0x10dxlxxbvmf05yymnnfkshy6vg";
   };
+  phases = "unpackPhase";
   shellHook = ''
-    export SPACEMACS_SRC=$src
+    echo $src
+    cp -v -R --no-preserve=mode $src ~/.emacs.d
+    ${builtins.toString copyPackages}
   '';
 }
